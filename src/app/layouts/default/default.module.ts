9649512cad48e39cede834c3a87296c98881886a
Robin Ochoa <robin8a@example.com>
2020-04-22 03:32:45 +0000

    Added material components to default.component.html

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -5,6 +5,7 @@ import { DashboardComponent } from '../../modules/dashboard/dashboard.component'
 import { RouterModule } from '@angular/router';
 import { PostsComponent } from '../../modules/posts/posts.component';
 import { SharedModule } from '../../shared/shared.module';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { SharedModule } from '../../shared/shared.module';
   imports: [
     CommonModule,
     RouterModule,
-    SharedModule
+    SharedModule,
+    MatSidenavModule
   ]
 })
 export class DefaultModule { }
